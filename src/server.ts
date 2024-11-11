9ded8bb3255941c3f2fbb953e2c7ef8cce16f251
Fumiya Kichikawa <kichikawa@acshu.co.jp>
2024-11-11 22:24:48 +0900

    保存

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import { config } from 'dotenv';
 
 import { createUser } from './controllers/CreateUser';
+import { getUser } from './controllers/GetUser';
 import { getUsers } from './controllers/GetUsers';
+import { updateUser } from './controllers/UpdateUser';
 
 config(); // 環境変数を読み込む
 
@@ -13,8 +15,10 @@ app.use(express.json());
 
 // ルーティング
 // TODO: 外だし
+app.get('/api/user', getUser);
 app.get('/api/users', getUsers);
 app.post('/api/users', createUser);
+app.put('/api/users', updateUser);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
